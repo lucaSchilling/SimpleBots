2773431a70c0441842522e08e37aeed0fdfb08ef
lucaSchilling <luca.schilling@bwedu.de>
2017-11-08 08:28:02 +0100

    selenium test expanded

--- botconfig/frontend/src/components/botconfig.vue
+++ botconfig/frontend/src/components/botconfig.vue
@@ -1,29 +1,29 @@
 <template>
 <div>
   <form novalidate @submit.stop.prevent="submit">
-  <md-input-container>
+  <md-input-container id="__id">
     <label>__id</label>
     <md-input v-model="__id"></md-input>
   </md-input-container>
 
 
-  <md-input-container>
+  <md-input-container id="template">
     <label>template</label>
     <md-input v-model="template"></md-input>
   </md-input-container>
 
-  <md-input-container>
+  <md-input-container id="name">
     <label>name</label>
     <md-input v-model="name"></md-input>
   </md-input-container>
 
-  <md-input-container>
+  <md-input-container id="lastEdit">
     <label>lastEdit</label>
     <md-input v-model="lastEdit"></md-input>
   </md-input-container>
 </form>
 
-<button v-on:click="deploy" v-on:keyup.13="deploy">Deploy</button>
+<button v-on:click="deploy" v-on:keyup.13="deploy" id="deployButton>Deploy</button>
 </div>
 </template>
 
@@ -42,7 +42,6 @@ export default {
   },
   methods: {
     deploy () {
-      alert('clicked')
       axios.post('http://141.19.142.6:3000/deploy', {
         '__id': this.__id,
         'template': this.template,
--- botconfig/frontend/test/e2e/Selenium_Test.js
+++ botconfig/frontend/test/e2e/Selenium_Test.js
@@ -5,15 +5,14 @@ var driver = new webdriver.Builder().forBrowser('chrome').build()
 
 driver.get('localhost:8080')
 driver.findElement(By.className('md-icon-button')).click()
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < 2; i++) {
   driver.findElement(By.id('HomeButton')).click()
   driver.findElement(By.id('TemplatesButton')).click()
+  driver.findElement(By.id('Botconfig')).click()
 }
 driver.findElement(By.className('md-sidenav-backdrop')).click()
-for (var j = 0; j < 10; j++) {
-  driver.findElement(By.id('showButton')).click()
-}
-driver.findElement(By.className('md-icon-button')).click()
-driver.findElement(By.id('HomeButton')).click()
-driver.findElement(By.className('md-sidenav-backdrop')).click()
-driver.findElement(By.className('md-icon-button')).click()
+
+driver.findElement(By.id('__id')).click()
+driver.findElement(By.id('__id')).click()
+
+driver.findElement(By.id('deployButton')).click()
