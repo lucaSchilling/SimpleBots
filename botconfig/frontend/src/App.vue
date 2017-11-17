3b24329ea36f67247025392f3e8234ddefc20b6c
Raphi1524694 <raphaellubaschewski@gmail.com>
2017-11-17 20:05:16 +0100

    i don't know what i'm doing

--- botconfig/frontend/src/App.vue
+++ botconfig/frontend/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-     <navigation> </navigation>
+     <tabs></tabs>
     <router-view/>
   </div>
 </template>
 
 <script>
-import navigation from './components/navigation.vue'
+import tabs from './components/tabs.vue'
 
 export default {
   components: {
-    navigation
+    tabs
   },
   name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- botconfig/frontend/src/main.js
+++ botconfig/frontend/src/main.js
@@ -2,9 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.css'
+import 'vue-material/dist/vue-material.min.css'
 import App from './App'
-import router from './router'
 
 Vue.use(VueMaterial)
 
@@ -13,7 +12,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   template: '<App/>',
   components: { App }
 })
