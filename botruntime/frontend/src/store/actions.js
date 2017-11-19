282084f373eb0017efac735175752029d3cbee25
Luca-Heim <luca.schilling@bwedu.de>
2017-11-19 20:15:43 +0100

    added docker-compose

--- botruntime/frontend/src/store/actions.js
+++ botruntime/frontend/src/store/actions.js
@@ -2,18 +2,18 @@ var axios = require('axios')
 
 export default {
   getAll: (context) => {
-    axios.get('http://141.19.142.6:3000/getAll').then((response) => {
+    axios.get('http://runtimebackend:3000/getAll').then((response) => {
       context.commit('getAll', response)
     })
   },
   removeBot: (context, id) => {
-    axios.delete('http://141.19.142.6:3000/delete/' + id
+    axios.delete('http://runtimebackend:3000/delete/' + id
       ).then(function (response) {
         context.dispatch('getAll')
       })
   },
   postStatus: (context, object) => {
-    axios.post('http://141.19.142.6:3000/setStatus', {
+    axios.post('http://runtimebackend:3000/setStatus', {
       '_id': object.id,
       'status': object.status
     }).then(function (response) {
