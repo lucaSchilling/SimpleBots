ab31c15daf80b5cd51f4d5c49d0e62ff6a7bd4d7
Ibrahim <i.dursun@outlook.de>
2017-11-21 22:03:49 +0100

    e2e deployment

--- botconfig/frontend/test/e2e/Selenium_Test2.js
+++ botconfig/frontend/test/e2e/Selenium_Test2.js
@@ -5,24 +5,37 @@ var By = webdriver.By
 
 var driver = new webdriver.Builder().forBrowser('chrome').build()
 
-//driver.get('http://141.19.142.6/dist/config/frontend/')
-driver.get('localhost:8080')
+driver.get('http://localhost:8080/')
 
-driver.findElement(By.id('tab-botarmy')).click()
-driver.findElement(By.id('next')).click()
-driver.findElement(By.CssSelector("md-step#second mdbutton")).click()
-//driver.findElement(By.CssSelector('cont')).click()
+driver.sleep(1000)
 
-/*
-driver.findElement(By.id('Botconfig')).click().then(function(){
+driver.findElement(By.id('tab-botarmy')).click().then(function(){
     return driver.getCurrentUrl();
 }).then (function(currentUrl){
-    if(currentUrl != 'http://141.19.142.6:8081/#/Botconfig'){
+    if(currentUrl != 'http://localhost:8080/'){
+        console.error("Stimmt nicht überein!");
+        console.log(currentUrl)
+}
+})
+driver.sleep(1000)
+
+driver.findElement(By.id('next')).click().then(function(){
+    return driver.getCurrentUrl();
+}).then (function(currentUrl){
+    if(currentUrl != 'http://localhost:8080/'){
         console.error("Stimmt nicht überein!");
 }
 })
+driver.sleep(1000)
 
-driver.findElement(By.className('md-sidenav-backdrop')).click()
+driver.findElement(By.id('continue')).click().then(function(){
+    return driver.getCurrentUrl();
+}).then (function(currentUrl){
+    if(currentUrl != 'http://localhost:8080/'){
+        console.error("Stimmt nicht überein!");
+}
+})
+driver.sleep(1000)
 
 var _id = Math.round (Math.random() * 1357)
 var welcomeMessage = ['Hello :)','Hi :)','HuHu :)','Willkommen :)','Ich bin bereit :)','Los geht´s!'][Math.floor(Math.random()*6)]
@@ -31,29 +44,60 @@ var template = 'Welcome Bot'
 var name = ['Bauer','Frank','Otto','Hans','Ronaldo','Schmidt',][Math.floor(Math.random()*6)]
 var lastEdit = new Date().getHours() +":" + new Date().getMinutes()
 
-var idF = driver.findElement(By.id('_id'))
+var idF = driver.findElement(By.id('id'))
 idF.click()
 idF.sendKeys(_id)
-
-var wmF = driver.findElement(By.id('welcomeMessage'))
-wmF.click()
-wmF.sendKeys(welcomeMessage);
-
-var oF = driver.findElement(By.id('options'))
-oF.click()
-oF.sendKeys(options);
-
-var tF = driver.findElement(By.id('template'))
-tF.click()
-tF.sendKeys(template);
+driver.sleep(1000)
 
 var nF = driver.findElement(By.id('name'))
 nF.click()
 nF.sendKeys(name);
+driver.sleep(1000)
+
+var wmF = driver.findElement(By.id('welcomeMessage'))
+wmF.click()
+wmF.sendKeys(welcomeMessage);
+driver.sleep(1000)
+
+var oF = driver.findElement(By.id('options'))
+oF.click()
+oF.sendKeys(options);
+driver.sleep(1000)
+
+var leF = driver.findElement(By.id('field'))
+leF.click()
+driver.sleep(1000)
+
+var faq = driver.findElement(By.id('FAQ'))
+faq.click() 
+driver.sleep(2000)
+
+leF.click()
+driver.sleep(1000)
+
+var taB = driver.findElement(By.id('taB'))
+taB.click() 
+driver.sleep(2000)
+
+leF.click()
+driver.sleep(1000)
+
+var wB = driver.findElement(By.id('wB'))
+wB.click() 
+driver.sleep(1000)
 
 var leF = driver.findElement(By.id('lastEdit'))
 leF.click()
 leF.sendKeys(lastEdit);
+driver.sleep(1000)
 
-driver.findElement(By.id('deployButton')).click()
+driver.findElement(By.id('deployButton')).click()/*.then(function(){
+    return driver.getCurrentUrl();
+}).then (function(currentUrl){
+    if(currentUrl != 'http://localhost:8080/'){
+        console.error("Stimmt nicht überein!");
+        console.log(currentUrl)
+}
+})
 */
+driver.sleep(1000)
