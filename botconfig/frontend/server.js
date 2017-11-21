f6b500c1174ff8ba54d5de0b56abf66d073af2a9
lucaSchilling <luca.schilling@bwedu.de>
2017-11-21 17:06:00 +0100

    reset

--- botconfig/frontend/server.js
+++ botconfig/frontend/server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8081
 const router = express.Router()
-app.use(express.static(`${__dirname}/`)) // set the static files location for the static html
+app.use(express.static(`${__dirname}`)) // set the static files location for the static html
 app.engine('.html', require('ejs').renderFile)
-app.set('views', `${__dirname}/`)
+app.set('views', `${__dirname}`)
 router.get('/*', (req, res, next) => {
   res.sendFile(`${__dirname}/index.html`)
 })
