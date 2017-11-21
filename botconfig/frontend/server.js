37963f44d75aaa1a8775f55a68c61620db841a71
lucaSchilling <luca.schilling@bwedu.de>
2017-11-21 17:18:07 +0100

    FIX THIS FUCKING SHIT

--- botconfig/frontend/server.js
+++ botconfig/frontend/server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8081
 const router = express.Router()
-app.use(express.static(`${__dirname}`)) // set the static files location for the static html
+app.use(express.static(`${__dirname}/dist`)) // set the static files location for the static html
 app.engine('.html', require('ejs').renderFile)
-app.set('views', `${__dirname}`)
+app.set('views', `${__dirname}/dist`)
 router.get('/*', (req, res, next) => {
-  res.sendFile(`${__dirname}/index.html`)
+  res.sendFile(`${__dirname}/dist/index.html`)
 })
 app.use('/', router)
 app.listen(port)
