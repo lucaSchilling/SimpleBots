402a5f964810f1830100104414ba3f70cb0687d8
Raphael Lubaschewski <raphaellubaschewski@gmail.com>
2017-11-23 09:33:35 +0100

    changed url in store to a variable

--- botconfig/frontend/src/store/actions.js
+++ botconfig/frontend/src/store/actions.js
@@ -1,8 +1,9 @@
 var axios = require('axios')
+var url = 'http://141.19.142.6:3000'
 
 export default {
   deploy: (context) => {
-    axios.post('http://141.19.142.6:3000/deploy', {
+    axios.post(url + '/deploy', {
       'template': context.state.template,
       'name': context.state.name,
       'welcomeMessage': context.state.welcomeMessage,
@@ -12,19 +13,19 @@ export default {
     })
   },
   getAll: (context) => {
-    axios.get('http://141.19.142.6:3000/getAll').then((response) => {
+    axios.get(url + '/getAll').then((response) => {
       context.commit('clearBotsFromArray')
       context.commit('getAll', response)
     })
   },
   delete: (context, id) => {
-    axios.delete('http://141.19.142.6:3000/delete/' + id
+    axios.delete(url + '/delete/' + id
     ).then(function (response) {
       context.dispatch('getAll')
     })
   },
   postStatus (context, object) {
-    axios.post('http://141.19.142.6:3000/setStatus', {
+    axios.post(url + '/setStatus', {
       '_id': object.id,
       'status': object.status
     }).then(function (response) {
