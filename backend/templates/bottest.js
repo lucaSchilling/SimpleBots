51255b6961c2d343c278f97f409cf4e876e838d2
ChristopherRotter <christopher.rotter@stud.hs-mannheim.de>
2017-11-27 12:09:46 +0100

    Added options

--- backend/templates/bottest.js
+++ backend/templates/bottest.js
@@ -3,5 +3,5 @@ var welcomebot = require('./welcomebot');
 var bot = new welcomebot("25352227", 
 "luca.schilling@bwedu.de", 
 "!Slytherin1g", 
-{_id: '1', name: 'Luca', options: ' ', welcomemessage: 'Hallo I Bims der Lucabot'})
+{_id: '1', name: 'Luca', options: [{message: 'opt1', redirect: ' '}, {message: 'opt2', redirect: ' '}, {message: 'opt3', redirect: ' '}], welcomemessage: 'Hallo I Bims der Lucabot'})
 bot.start();
