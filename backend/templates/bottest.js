22a8731cc21e531d214b01c18c2d1638ad99767e
ChristopherRotter <christopher.rotter@stud.hs-mannheim.de>
2017-11-27 11:33:34 +0100

    Fix

--- backend/templates/bottest.js
+++ backend/templates/bottest.js
@@ -1,4 +1,4 @@
-var welcomebot = require('./welcomebot.js');
+var welcomebot = require('./welcomebot');
 
 var bot = new welcomebot("25352227", "luca.schilling@bwedu.de", "!Slytherin1g", "{'_id': '1', 'name': 'Luca', 'options': '', 'welcomemessage': 'Hallo I Bims der Lucabot'}")
 bot.start();
