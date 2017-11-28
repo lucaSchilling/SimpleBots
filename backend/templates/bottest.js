7bbcc2b230204264cbfb425e0d8508ee231a2d90
lucaSchilling <luca.schilling@bwedu.de>
2017-11-28 13:16:49 +0100

    fixed json parsing

--- backend/templates/bottest.js
+++ backend/templates/bottest.js
@@ -6,12 +6,10 @@ for (let j = 0; j < process.argv.length; j++){
     console.log(j + ' -> ' + process.argv[j])
 }
 
-var bot = new welcomebot("25352227", 
+var bot = new welcomebot(
+    "25352227", 
 "christopher", 
 "!Slytherin1g", 
-config._id,
-config.name,
-config.welcomeMessage,
-config.options
+config
 )
 bot.start();
