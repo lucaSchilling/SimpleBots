398daef2c9e9b82acf8ec577cfb3ba5970b77b23
lucaSchilling <luca.schilling@bwedu.de>
2017-11-30 11:06:43 +0100

    giving id as parameter and red config from database by id

--- backend/templates/bottest.js
+++ backend/templates/bottest.js
@@ -1,15 +1,49 @@
 var welcomebot = require('./welcomebot');
-var config = JSON.parse(process.argv[2]); 
+var id = process.argv[2]; 
+// MongoDB module
+var db = require('./db');
+let mongoURL ='mongodb://127.0.0.1/runtimedb'
+const promisify = require("es6-promisify")
 
-
+var currentConfig;
 for (let j = 0; j < process.argv.length; j++){
     console.log(j + ' -> ' + process.argv[j])
 }
+console.log(id)
 
-var bot = new welcomebot(
-    "25352227", 
-"christopher", 
-"!Slytherin1g", 
-config
-)
-bot.start();
+db.connect(mongoURL, function(err) {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    else {
+        // Load existing bots
+        return new Promise((resolve, reject) => {
+         db.get().collection('deployedBots').find({}).toArray(function(err, result){
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+
+            if (result) {
+                for (let config of result) {
+                    if(config._id===id){
+                        console.log('Hallo')
+                        console.log(config)
+                        resolve(config)
+                    }else{
+                        console.log('Nicht die korrekte Config')
+                    }
+                }
+                reject()
+            }
+        })
+    }).then(function(config){
+    console.log('versuche Bot zu starten')
+    var bot = new welcomebot ('25352227', 'christopher', '!Slytherin1g', config);
+    bot.start();
+    console.log('Bot gestartet')
+    }).catch(function () {
+    console.log("Promise Rejected");
+    })
+    }})
