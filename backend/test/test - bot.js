913703079a65b4b073af7170603a5d1e92d470dd
Ibrahim <i.dursun@outlook.de>
2017-12-07 13:31:39 +0100

    bot, wbot updated

--- backend/test/test - bot.js
+++ backend/test/test - bot.js
@@ -1,18 +1,23 @@
+
 const { config } = require('dotenv');
 var botClass = require('../templates/bot');
 var assert = require('chai').assert;
+var db = require('../db')
 
 config();
 var accountId = process.env.LP_ACCOUNT;
 var username = process.env.LP_USER;
 var password = process.env.LP_PASS;
 var port = process.env.PORT;
-var csds = process.env.LP_CSDS;
+var MONGOURL = 'mongodb:141.19.142.6:27017/testdb';
 
-describe.skip('### Bot - Setting up a bot ###', function() {
-    
-    var bot = new botClass(accountId, username, password, csds, config);
-    
+describe('### Bot - Setting up a bot ###', function() {
+
+    // console.log("Hey I bims: " + config)
+    // console.log("Hey I bims2: " + config.options)
+
+    var bot = new botClass(accountId, username, password, {name:'Lucas stinkt2!', welcomeMessage:'Hey', options: [{message: 'Naa', redirect: '1'}]});
+   
     it('create a bot', function() {
         assert.equal(bot.accountId, '25352227')
         assert.equal(bot.username, 'luca.schilling@bwedu.de')
--- backend/test/test - wbot.js
+++ backend/test/test - wbot.js
@@ -3,17 +3,18 @@ var botClass = require('../templates/bot');
 var welcomeBotClass = require('../templates/welcomebot');
 var assert = require('chai').assert;
 
+
 config();
 var accountId = process.env.LP_ACCOUNT;
 var username = process.env.LP_USER;
 var password = process.env.LP_PASS;
 var port = process.env.PORT;
-var csds = process.env.LP_CSDS;
+var MONGOURL = 'mongodb:141.19.142.6:27017/testdb';
 
-describe.skip('### WelcomeBot - Setting up a bot ###', function() {
+describe('### WelcomeBot - Setting up a bot ###', function() {
     
-    var bot = new botClass(accountId, username, password, csds, config);
-    
+    var bot = new welcomeBotClass(accountId, username, password, {name:'Lucas stinkt!', welcomeMessage:'Hey', options: [{message: 'Naa', redirect: '1'}]});
+
     it('create a bot', function() {
         assert.equal(bot.accountId, '25352227')
         assert.equal(bot.username, 'luca.schilling@bwedu.de')
