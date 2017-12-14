65025066c5c1ed22b24edcb150a33e2b4eadfef9
lucaSchilling <luca.schilling@bwedu.de>
2017-12-14 09:10:03 +0100

    fixed frontend port

--- frontend/server.js
+++ frontend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080
+const port = 8080
 const router = express.Router()
 app.use(express.static(`${__dirname}/dist`)) // set the static files location for the static html
 app.engine('.html', require('ejs').renderFile)
