96fd5297f383d13fa2726154288777ede843c07c
lucaSchilling <luca.schilling@bwedu.de>
2017-12-15 10:49:18 +0100

    Revert "Revert "Revert "Merge pull request #46 from lucaSchilling/developer-refactoring"""
    This reverts commit 24e9276c4c419f66ae86b707dd77cf95e4900f42.

--- backend/templates/bot.js
+++ backend/templates/bot.js
@@ -1,9 +1,7 @@
-// Node Agent SDK module
 const { Agent } = require('node-agent-sdk');
+// Used to transform the existing callback based functions into promise based functions
+const { promisify } = require('util');
 
-/**
- * A generic bot that uses the Node Agent SDK to connect to the LiveEngage platform.
- */
 class Bot {
     /**
      * Creates a new bot that can join conversations via the specified agent.
@@ -160,9 +158,10 @@ class Bot {
      * @param {string} conversationId The id of the conversation
      */
     async leaveConversation(conversationId) {
+        console.log('Bot ' + this.config._id + ' has left conversation ' + conversationId)
         delete this.openConversations[conversationId];
 
-        await this.agent.updateConversationField({
+        return await this.agent.updateConversationField({
             'conversationId': conversationId,
             'conversationField': [{
                 field: 'ParticipantsChange',
@@ -170,15 +169,9 @@ class Bot {
                 role: 'MANAGER'
             }]
         });
-
-        console.log('Left conversation ' + conversationId);
     }
     
-    /**
-     * Halts the code execution for the given amout of milliseconds.
-     * @param {int} ms The delay in milliseconds.
-     */
-    timeout(ms) {
+    timeout(ms = 3000) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 }
