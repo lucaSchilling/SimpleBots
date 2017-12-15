039efa9db8a65e5799cc837da6686c6fa27b94d6
lucaSchilling <luca.schilling@bwedu.de>
2017-12-15 08:59:39 +0100

    added comments and fixed some shit

--- backend/templates/botStart.js
+++ backend/templates/botStart.js
@@ -1,9 +1,10 @@
-// Gets the id from the run parameters
+// gets the id from the run parameters
 let id = process.argv[2];
 console.log(id)
+// gets the username from the run parameters
 let username = process.argv[3]
 console.log(username)
-// MongoDB module
+// mongoDB module
 const db = require('./db');
 var mongoURL = 'mongodb://database:27017/' + username;
 
@@ -14,7 +15,7 @@ db.connect(mongoURL, function (err) {
     }
     else {
         return new Promise((resolve, reject) => {
-            // Gets the config for this bot
+            // gets the config for this bot
             db.get().collection('deployedBots').findOne({ _id: id }, function (err, result) {
                 if (err) {
                     console.error('Error' + err);
@@ -28,9 +29,9 @@ db.connect(mongoURL, function (err) {
                 reject()
             })
         }).then(function (config) {
-            // Requires the correct file for the needed template
+            // requires the correct file for the needed template
             let template = require('./' + config.template.replace(' ', '').toLowerCase())
-            // Creates a new bot of the needed template with his config
+            // creates a new bot of the needed template with his config
             let bot = new template('25352227', 'christopher', '!Slytherin1g', config);
             bot.start();
             console.log('Bot gestartet')
--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+// use either the .env port or 8080 as port
 const port = process.env.PORT || 8080
 const router = express.Router()
-app.use(express.static(`${__dirname}/dist`)) // set the static files location for the static html
+// set the static files location for the static html
+app.use(express.static(`${__dirname}/dist`))
 app.engine('.html', require('ejs').renderFile)
 app.set('views', `${__dirname}/dist`)
+// give the requester the correct file from our static files folder
 router.get('/*', (req, res, next) => {
   res.sendFile(`${__dirname}/dist/index.html`)
 })
